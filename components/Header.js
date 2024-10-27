6401b707e68ec063eab518b4fe8fd4e574f20734
xzddf <125452471+xzddf@users.noreply.github.com>
2024-10-27 15:33:00 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,9 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name: Youtube, to: 'https://youtube.com/@xzddf', show: true },
-    { id: 1, name: Twitter, to: 'https://x.com/xzddfcom', show: true },
-    { id: 2, name: Telegram, to: 'https://t.me/xzddfcom', show: true },
+    { id: 0, name:'Youtube', to: 'https://youtube.com/@xzddf', show: true },
+    { id: 1, name:'Twitter', to: 'https://x.com/xzddfcom', show: true },
+    { id: 2, name:'Telegram', to: 'https://t.me/xzddfcom', show: true },
     
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
