e5bf15c88f67a0d6660e54b9d3eb48d788c8f350
xzddf <125452471+xzddf@users.noreply.github.com>
2024-10-27 16:09:06 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,11 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name:'Youtube', to: 'https://youtube.com/@xzddf', show: true },
-    { id: 1, name:'Telegram', to: 'https://t.me/xzddfcom', show: true },
+    { id: 0, name:'YT', to: 'https://youtube.com/@xzddf', show: true },
+    { id: 1, name:'X', to: 'https://x.com/xzddfcom', show: true },
+    { id: 2, name:'TG', to: 'https://t.me/xzddfcom', show: true },
     
-    { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
