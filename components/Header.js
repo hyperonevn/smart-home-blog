f8a983f69ef83a235efc1945209e441ee48efdf5
xzddf <125452471+xzddf@users.noreply.github.com>
2025-05-11 00:02:07 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,10 +10,10 @@ const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name:'YouTube', to: 'https://youtube.com/@yinomad', show: true },
-    { id: 1, name:'X', to: 'https://x.com/xzddfcom', show: flase },
-    { id: 2, name:'TG', to: 'https://t.me/xzddfcom', show: flase },
+    { id: 1, name:'X', to: 'https://x.com/xzddfcom', show: false },
+    { id: 2, name:'TG', to: 'https://t.me/xzddfcom', show: false },
     
-    { id: 3, name: locale.NAV.RSS, to: '/feed', show: true, external: flase },
+    { id: 3, name: locale.NAV.RSS, to: '/feed', show: true, external: false },
     { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
