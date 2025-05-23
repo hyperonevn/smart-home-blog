5f31b90a7155299a4528bc0b68d236707d26786f
xzddf <125452471+xzddf@users.noreply.github.com>
2025-05-23 12:47:50 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -11,8 +11,8 @@ const NavBar = () => {
   const links = [
     { id: 0, name:'YouTube', to: 'https://youtube.com/@xzddf', show: false },
     { id: 1, name:'X（Twitter）', to: 'https://x.com/xzddfcom', show: true },
-    { id: 2, name:'TG', to: 'https://t.me/xzddfcom', show: true },
-    { id: 3, name:'Email', to: 'mailto:xzddf@duck.com', show: true },
+    { id: 2, name:'频道', to: 'https://t.me/xzddfcom', show: true },
+    { id: 3, name:'联系', to: 'mailto:xzddf@duck.com', show: true },
     
     { id: 4, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
     { id: 5, name:'搜索', to: '/search', show: true }
