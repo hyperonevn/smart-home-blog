54105b76e2c2e37912a637f9fa5192f63e7df19f
xzddf <125452471+xzddf@users.noreply.github.com>
2025-05-27 11:08:32 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,10 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name:'YouTube', to: 'https://youtube.com/@xzddf', show: false },
-    { id: 1, name:'X（Twitter）', to: 'https://x.com/laozeieth', show: true },
+    { id: 0, name:'YouTube', to: 'https://youtube.com/@laozei', show: false },
+    { id: 1, name:'X（Twitter）', to: 'https://x.com/laozeieth', show: false },
     { id: 2, name:'频道', to: 'https://t.me/xzddfcom', show: false },
-    { id: 3, name:'联系', to: 'mailto:xzddf@duck.com', show: false },
+    { id: 3, name:'联系', to: 'https://linktr.ee/laozei', show: true },
     
     { id: 4, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
     { id: 5, name:'搜索', to: '/search', show: true }
