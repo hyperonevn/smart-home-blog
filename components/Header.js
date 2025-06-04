35b7b864f887d1303df750fbb3ba62ef92bed65d
laozei <125452471+laozeixyz@users.noreply.github.com>
2025-06-04 12:44:43 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,8 +10,8 @@ const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name:'YouTube', to: 'https://youtube.com/@laozei', show: false },
-    { id: 1, name:'X（Twitter）', to: 'https://x.com/laozeieth', show: true },
-    { id: 2, name:'频道', to: 'https://t.me/xzddfcom', show: false },
+    { id: 1, name:'X（Twitter）', to: 'https://x.com/laozeixyz', show: true },
+    { id: 2, name:'频道', to: 'https://t.me/laozei_xyz', show: false },
     { id: 3, name:'联系', to: 'https://linktr.ee/laozei', show: false },
     
     { id: 4, name: locale.NAV.RSS, to: '/feed', show: false, external: true },
